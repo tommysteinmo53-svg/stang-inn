661cd9b3c1521fdcce733e96f84abd9ee94570b9
tommysteinmo53-svg <tommysteinmo53@gmail.com>
2026-08-14 18:18:59 +0200

    ui(fantasy): load team readiness styles

--- app/fantasy/layout.tsx
+++ app/fantasy/layout.tsx
@@ -2,6 +2,7 @@ import FantasyNav from "./FantasyNav";
 import "./fantasy-nav.css";
 import "./leaderboard/leaderboard.css";
 import "./rounds/rounds.css";
+import "./team/team-launch.css";
 
 export default function FantasyLayout({children}:{children:React.ReactNode}){
  return <><FantasyNav/>{children}</>;
