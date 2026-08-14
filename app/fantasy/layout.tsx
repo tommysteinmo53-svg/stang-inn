cec06f147bbc8607783e1a8d45b57e425deb841d
tommysteinmo53-svg <tommysteinmo53@gmail.com>
2026-08-14 18:24:22 +0200

    Use shared auth gate for fantasy player flow

--- app/fantasy/layout.tsx
+++ app/fantasy/layout.tsx
@@ -1,9 +1,10 @@
 import FantasyNav from "./FantasyNav";
+import FantasyAuthGate from "./FantasyAuthGate";
 import "./fantasy-nav.css";
 import "./leaderboard/leaderboard.css";
 import "./rounds/rounds.css";
 import "./team/team-launch.css";
 
 export default function FantasyLayout({children}:{children:React.ReactNode}){
- return <><FantasyNav/>{children}</>;
+ return <FantasyAuthGate><FantasyNav/>{children}</FantasyAuthGate>;
 }
