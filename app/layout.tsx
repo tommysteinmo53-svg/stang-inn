500bc1ecd473da26529daea8cb7237695750f3b5
tommysteinmo53-svg <tommysteinmo53@gmail.com>
2026-08-14 18:43:01 +0200

    Show global Hockeytips Fantasy switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./premium-next-match.css";
 import "./round-carousel.css";
 import "./home-live-table.css";
 import "./awards.css";
+import "./game-mode-switch.css";
 import AuthGate from "../components/AuthGate";
 import AuthReturnRedirect from "../components/AuthReturnRedirect";
 import AnnouncementBanner from "../components/AnnouncementBanner";
@@ -12,6 +13,7 @@ import GlobalMobileNav from "../components/GlobalMobileNav";
 import TopStatusBar from "../components/TopStatusBar";
 import PremiumNextMatch from "../components/PremiumNextMatch";
 import HomeLiveTable from "../components/HomeLiveTable";
+import GameModeSwitch from "../components/GameModeSwitch";
 
 export const metadata = {
   title: "Stang Inn",
@@ -25,6 +27,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <AuthReturnRedirect />
         <TopStatusBar />
         <AuthGate>
+          <GameModeSwitch />
           <PremiumNextMatch />
           <HomeLiveTable />
           {children}
