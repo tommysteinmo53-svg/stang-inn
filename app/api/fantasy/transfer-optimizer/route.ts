c506a2dbdf475dc4d9e470f2074b9632fc751172
tommysteinmo53-svg <tommysteinmo53@gmail.com>
2026-08-23 15:34:24 +0200

    Remove public optimizer API alias

--- app/api/fantasy/transfer-optimizer/route.ts
+++
@@ -1,9 +0,0 @@
-import type {NextRequest} from "next/server";
-import {GET as optimizerGet} from "../../admin/fantasy/transfer-optimizer/route";
-
-export const runtime="nodejs";
-export const dynamic="force-dynamic";
-
-export async function GET(request:NextRequest){
-  return optimizerGet(request);
-}
