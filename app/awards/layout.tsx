89b4fc98862c7af5c815255d820db00cceccef87
tommysteinmo53-svg <tommysteinmo53@gmail.com>
2026-08-25 10:47:52 +0200

    fix(mp-11.8): remove full viewport wrapper from tipping statistics nav

--- app/awards/layout.tsx
+++ app/awards/layout.tsx
@@ -1,5 +1,5 @@
 import TippingSectionNav from "../../components/TippingSectionNav";
 
 export default function AwardsLayout({children}:{children:React.ReactNode}){
- return <><div className="appShell tippingHubNavShell"><TippingSectionNav/></div>{children}</>;
+ return <><div className="tippingHubNavShell"><TippingSectionNav/></div>{children}</>;
 }
