52b46fd7832adf1d0f3d3df62f0b8e535d6f2bc7
tommysteinmo53-svg <tommysteinmo53@gmail.com>
2026-08-25 10:45:33 +0200

    feat(mp-11.8): expose tipping navigation on statistics

---
+++ app/awards/layout.tsx
@@ -0,0 +1,5 @@
+import TippingSectionNav from "../../components/TippingSectionNav";
+
+export default function AwardsLayout({children}:{children:React.ReactNode}){
+ return <><div className="appShell tippingHubNavShell"><TippingSectionNav/></div>{children}</>;
+}
