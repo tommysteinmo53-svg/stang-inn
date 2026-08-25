f64b63d042c6b3b2ac890096c2037b8fb829561a
tommysteinmo53-svg <tommysteinmo53@gmail.com>
2026-08-25 20:41:38 +0200

    style(fantasy): enable final consistency layer

--- app/fantasy/layout.tsx
+++ app/fantasy/layout.tsx
@@ -5,6 +5,7 @@ import "./leaderboard/leaderboard.css";
 import "./rounds/rounds.css";
 import "./team/team-launch.css";
 import "./fantasy-premium.css";
+import "./fantasy-final-polish.css";
 
 export default function FantasyLayout({children}:{children:React.ReactNode}){
  return <FantasyAuthGate><FantasyNav/>{children}</FantasyAuthGate>;
