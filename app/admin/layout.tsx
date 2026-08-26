10bb6011caf037bfd2baa1a00866895fc4254ffd
tommysteinmo53-svg <tommysteinmo53@gmail.com>
2026-08-26 10:40:10 +0200

    feat(admin): link shared user administration

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -7,6 +7,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
       <nav className="adminTopNav" aria-label="Adminmeny">
         <div className="adminTopNavInner">
           <a href="/admin"><SIIcon name="overview" size={18}/> <span>Admin</span></a>
+          <a href="/admin/users"><SIIcon name="player" size={18}/> <span>Brukere</span></a>
           <a href="/admin/fantasy"><SIIcon name="fantasy" size={18}/> <span>Fantasyhockey</span></a>
           <a href="/admin/hockeytips"><SIIcon name="tips" size={18}/> <span>Hockeytipset</span></a>
         </div>
